891b1e1af31344a313c34b4f6276ea4e6a25021e
BrunoCanepa <brunocanepa22@gmail.com>
2022-12-16 08:41:58 -0300

    Comentario

--- src/Library/IPrinter.cs
+++ src/Library/IPrinter.cs
@@ -1,5 +1,6 @@
 namespace Full_GRASP_And_SOLID.Library;
-
+//Se utiliza el patrón Polimorfismo ya que en vez de tener un bloque de código para escribir en consola y 
+//otro para escribir en un archivo, tiene dos clases distintas para cada acción
 public interface IPrinter
 {
     public void PrintBuilding(Building building);
